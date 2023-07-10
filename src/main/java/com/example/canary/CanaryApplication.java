ecd5dcdcdf3a1d1a6bc4d298b0df79a2905ca46a
hongliang <15711385916@163.com>
2023-07-11 00:26:36 +0800

    refact:时区调整为GMT+8

--- src/main/java/com/example/canary/CanaryApplication.java
+++ src/main/java/com/example/canary/CanaryApplication.java
@@ -1,15 +1,29 @@
 package com.example.canary;
 
+import jakarta.annotation.PostConstruct;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
+/**
+ * @author zhaohongliang
+ */
 @MapperScan("com.example.canary.*.mapper")
 @SpringBootApplication
 public class CanaryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CanaryApplication.class, args);
+	}
+
+	/**
+	 * 设置时区
+	 */
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("GMT+8"));
 	}
 
 }
