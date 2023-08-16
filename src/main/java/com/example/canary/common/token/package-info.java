e19df6d3321d04a291d4a091ef262a80955eeaa7
hongliang <15711385916@163.com>
2023-08-16 21:03:45 +0800

    feat:增加token的pakage-info.java

---
+++ src/main/java/com/example/canary/common/token/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * package-info
+ *
+ * @author zhaohongliang 2023-08-02 22:33
+ * @since 1.0
+ */
+// @NonNullApi 解决 Not annotated parameter overrides @NonNullApi parameter
+package com.example.canary.common.token;
