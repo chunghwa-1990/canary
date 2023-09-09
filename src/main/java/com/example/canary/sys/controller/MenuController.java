9a6fc715b5e54d2e7c28f2fceb29ab59b24a3c18
hongliang <15711385916@163.com>
2023-09-09 13:08:09 +0800

    feat:菜单增/删/改

--- src/main/java/com/example/canary/sys/controller/MenuController.java
+++ src/main/java/com/example/canary/sys/controller/MenuController.java
@@ -2,10 +2,18 @@ package com.example.canary.sys.controller;
 
 import com.example.canary.common.api.ApiVersion;
 import com.example.canary.common.exception.ResultEntity;
+import com.example.canary.common.exception.ValidGroup;
+import com.example.canary.sys.entity.MenuAO;
 import com.example.canary.sys.entity.MenuQuery;
 import com.example.canary.sys.entity.MenuVO;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,6 +39,43 @@ public class MenuController {
     public ResultEntity<MenuVO> pagesMenu(MenuQuery query) {
         return null;
     }
+
+    /**
+     * update
+     *
+     * @param menuAo
+     * @return
+     */
+    @PostMapping("/save")
+    @SuppressWarnings("rawtypes")
+    public ResultEntity saveMenu(@Validated({ ValidGroup.Add.class }) @RequestBody MenuAO menuAo) {
+        return null;
+    }
+
+    /**
+     * update
+     *
+     * @param menuAo
+     * @return
+     */
+    @PutMapping("/update")
+    @SuppressWarnings("rawtypes")
+    public ResultEntity updateMenu(@Validated({ ValidGroup.Edit.class }) @RequestBody MenuAO menuAo) {
+        return null;
+    }
+
+    /**
+     * delete
+     *
+     * @param menuId
+     * @return
+     */
+    @DeleteMapping("/delete")
+    @SuppressWarnings("rawtypes")
+    public ResultEntity deleteMenu(@NotNull String menuId) {
+        return null;
+    }
+
 
 
 }
