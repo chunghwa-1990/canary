eae8ee585f6aa31eed401066a6bed53198f1ceee
hongliang <15711385916@163.com>
2023-09-14 23:50:06 +0800

    fix:JSR-303 validation

--- src/main/java/com/example/canary/sys/entity/MenuBase.java
+++ src/main/java/com/example/canary/sys/entity/MenuBase.java
@@ -1,7 +1,16 @@
 package com.example.canary.sys.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.example.canary.common.exception.ValidGroup;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 /**
@@ -13,7 +22,55 @@ import java.io.Serializable;
 @Data
 public class MenuBase implements Serializable {
 
+    @Serial
     private static final long serialVersionUID = 3277512367742736079L;
+
+    /**
+     * id
+     */
+    @TableId(type = IdType.ASSIGN_UUID)
+    @NotBlank(groups = ValidGroup.Edit.class)
+    private String id;
+
+    /**
+     * 父菜单id
+     */
+    private String parentId;
+
+    /**
+     * 名称
+     */
+    @NotBlank(groups = { ValidGroup.Add.class, ValidGroup.Edit.class })
+    @Length(min = 1, max = 20, groups = { ValidGroup.Add.class, ValidGroup.Edit.class })
+    private String name;
+
+    /**
+     * 图标
+     */
+    private String icon;
+
+    /**
+     * 路由
+     */
+    @NotBlank(groups = { ValidGroup.Add.class, ValidGroup.Edit.class })
+    @Length(min = 1, max = 100, groups = { ValidGroup. Add.class, ValidGroup.Edit.class })
+    private String route;
+
+    /**
+     * 菜单级别 [0,1]
+     */
+    @Range(min = 1, max = 2)
+    private Integer level;
+
+    /**
+     * 是否禁用
+     * @see com.example.canary.common.enums.StatusEnum.Disabled
+     */
+    @Range(min = 0, max = 1, groups = { ValidGroup.Add.class, ValidGroup.Edit.class })
+    @TableField(value = "is_disabled")
+    private Integer disabled;
+
+
 
 
 }
--- src/main/java/com/example/canary/sys/entity/PermissionBase.java
+++ src/main/java/com/example/canary/sys/entity/PermissionBase.java
@@ -1,6 +1,11 @@
 package com.example.canary.sys.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.example.canary.common.exception.ValidGroup;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -20,12 +25,17 @@ public class PermissionBase implements Serializable {
     /**
      * id
      */
+    @TableId(type = IdType.ASSIGN_UUID)
+    @NotBlank(groups = ValidGroup.Edit.class)
     private String id;
 
     /**
      * name
      */
+    @NotBlank(groups = { ValidGroup.Add.class, ValidGroup.Edit.class })
+    @Length(min = 1, max = 20, groups = { ValidGroup.Add.class, ValidGroup.Edit.class })
     private String name;
+
 
 
 }
