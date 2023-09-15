a1ec7c7057613eacabc2ebc7ded0dd9a1fa2a07f
hongliang <15711385916@163.com>
2023-09-15 14:45:51 +0800

    fix:JSR-303 validation

--- src/main/java/com/example/canary/sys/entity/PermissionBase.java
+++ src/main/java/com/example/canary/sys/entity/PermissionBase.java
@@ -1,11 +1,13 @@
 package com.example.canary.sys.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.example.canary.common.exception.ValidGroup;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -30,12 +32,36 @@ public class PermissionBase implements Serializable {
     private String id;
 
     /**
+     * 菜单id
+     */
+    @NotBlank(groups = { ValidGroup.Add.class, ValidGroup.Edit.class })
+    private String menuId;
+
+    /**
      * name
      */
     @NotBlank(groups = { ValidGroup.Add.class, ValidGroup.Edit.class })
     @Length(min = 1, max = 20, groups = { ValidGroup.Add.class, ValidGroup.Edit.class })
     private String name;
 
+    /**
+     * 图标
+     */
+    private String icon;
 
+    /**
+     * 路由
+     */
+    @NotBlank(groups = { ValidGroup.Add.class, ValidGroup.Edit.class })
+    @Length(min = 1, max = 100, groups = { ValidGroup. Add.class, ValidGroup.Edit.class })
+    private String route;
+
+    /**
+     * 是否禁用
+     * @see com.example.canary.common.enums.StatusEnum.Disabled
+     */
+    @Range(min = 0, max = 1, groups = { ValidGroup.Add.class, ValidGroup.Edit.class })
+    @TableField(value = "is_disabled")
+    private Integer disabled;
 
 }
