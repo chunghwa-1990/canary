86472dac7ce218966f00efacc6956c794491da97
hongliang <15711385916@163.com>
2024-03-25 22:11:51 +0800

    feat:初始化脚本优化

--- init.sh
+++ init.sh
@@ -1,12 +1,53 @@
 #!/bin/bash
 
+# 目录
+HOME="/Users/zhaohongliang/DockerData"
+PROJECT="/Users/zhaohongliang/Github/canary"
+SQL="/Users/zhaohongliang/Desktop"
+
 # 命令行工具
 COMMAND_TO_DOCKER="docker"
 COMMAND_TO_MYSQL="mysql"
 COMMAND_TO_WGET="wget"
 COMMAND_TO_CURL="curl"
 
-# 检测docker命令是否存在
+# 定义旋转的光标符号
+spinner="/-\|"
+# 旋转光标
+dynamic_cursor() {
+    local j=0
+    local max=$1
+    local str=$2
+
+    while [ $j -le $max ]; do
+        for i in $(seq 0 3); do
+            printf "\r${str}${spinner:$i:1}"   # 使用回车符（\r）实现光标回到行首
+            sleep 0.2
+        done
+        j=`expr 1 + $j`
+    done
+    printf "\r\033[K"
+    printf "\n执行完毕！\n"
+}
+
+# 进度条
+progress() {
+    local i=0
+    local b=' '
+    local max=$1
+    local str=$2
+
+    while [ $i -le $max  ]; do
+        printf "${str}：[%-100s] %d%%\r" $b $i
+        sleep 0.1
+        i=`expr 1 + $i`
+        b=#$b
+    done
+    printf "\n${str} build successfully!\n"
+}
+
+
+
 if !command -v $COMMAND_TO_DOCKER &> /dev/null
 then
     echo "$COMMAND_TO_DOCKER 未安装，脚本中断执行"
@@ -22,26 +63,123 @@ fi
 
 # 镜像名称
 IMAGE_NAME_MYSQL="mysql:8.0"
+IMAGE_NAME_PROXYSQL="proxysql/proxysql"
 
 # 如果mysql镜像不存在，则拉取
 if !docker image inspect $IMAGE_NAME_MYSQL &> /dev/null
 then
     docker pull mysql:8.0
+    if [ $? -eq 0 ]; then
+        echo "$IMAGE_NAME_MYSQL 拉取完成"
+    fi
 fi
 
-# 创建网络
-docker network create canary-net
+# 如果proxy镜像不存在，则拉取
+if !docker image inspect $IMAGE_NAME_PROXYSQL &> /dev/null
+then
+    docker pull proxysql/proxysql
+    if [ $? -eq 0 ]; then
+        echo "$IMAGE_NAME_PROXYSQL 拉取完成"
+    fi
+fi
 
-sleep 5
+# 网络
+NETWORK_NAME="canary-net"
+if docker network ls | grep -q $NETWORK_NAME; then
+    while true; do
+        read -p "$NETWORK_NAME 已存在，如需覆盖请确认？（y/n）" choice
+        choice_lower=$(echo "$choice" | tr '[:upper:]' '[:lower:]')
+        if [ "$choice_lower" == "y" ] || [ "$choice_lower" == "yes" ]; then
+            docker network rm $NETWORK_NAME && docker network create $NETWORK_NAME
+            break
+        elif [ "$choice_lower" == "n" ] || [ "$choice_lower" == "no" ]; then
+            break
+        else
+            echo "输入不合法，请重新输入"
+        fi
+    done
+else
+    docker network create $NETWORK_NAME
+fi
+
+MYSQL_HOME="$HOME/mysql"
+MYSQL_MASTER_CNF="$MYSQL_HOME/mysql-master/conf/my.cnf"
+MYSQL_SLAVE_1_CNF="$MYSQL_HOME/mysql-slave-1/conf/my.cnf"
+MYSQL_SLAVE_2_CNF="$MYSQL_HOME/mysql-slave-2/conf/my.cnf"
+
+if [ ! -d "$MYSQL_HOME" ]; then 
+    # wget -P $MYSQL_HOME/mysql-master/conf  https://github.com/hahapigs/canary/blob/main/mysql-master.cnf 
+    mkdir -p $MYSQL_HOME/mysql-master/conf
+    mkdir -p $MYSQL_HOME/mysql-slave-1/conf
+    mkdir -p $MYSQL_HOME/mysql-slave-2/conf
+    cp -f $PROJECT/mysql-master.cnf $HOME/mysql/mysql-master/conf
+    cp -f $PROJECT/mysql-slave-1.cnf $HOME/mysql/mysql-slave-1/conf
+    cp -f $PROJECT/mysql-slave-2.cnf $HOME/mysql/mysql-slave-2/conf
+else
+    if [ ! -f "$MYSQL_MASTER_CNF" ]; then
+        # wget -P $HOME/mysql/mysql-master/conf  https://github.com/hahapigs/canary/blob/main/mysql-master.cnf 
+        mkdir -p $HOME/mysql/mysql-master/conf
+        cp -f $PROJECT/mysql-master.cnf $HOME/mysql/mysql-master/conf/my.cnf
+    else
+        while true; do
+            read -p "mysql-master 的 my.cnf 文件已存在，如需覆盖请确认？（y/n）" choice
+            choice_lower=$(echo "$choice" | tr '[:upper:]' '[:lower:]')
+            if [ "$choice_lower" == "y" ] || [ "$choice_lower" == "yes" ]; then
+                cp -f $PROJECT/mysql-master.cnf $HOME/mysql/mysql-master/conf/my.cnf
+                break
+            elif [ "$choice_lower" == "n" ] || [ "$choice_lower" == "no" ]; then
+                break
+            else
+                echo "输入不合法，请重新输入"
+            fi
+        done
+    fi
+
+    if [ ! -f "$MYSQL_SLAVE_1_CNF" ]; then
+        mkdir -p $HOME/mysql/mysql-slave-1/conf
+        cp -f $PROJECT/mysql-slave-1.cnf $HOME/mysql/mysql-slave-1/conf/my.cnf
+    else
+        while true; do
+            read -p "mysql-slave-1 的 my.cnf 文件已存在，如需覆盖请确认？（y/n）" choice
+            choice_lower=$(echo "$choice" | tr '[:upper:]' '[:lower:]')
+            if [ "$choice_lower" == "y" ] || [ "$choice_lower" == "yes" ]; then
+                cp -f $PROJECT/mysql-slave-1.cnf $HOME/mysql/mysql-slave-1/conf/my.cnf
+                break
+            elif [ "$choice_lower" == "n" ] || [ "$choice_lower" == "no" ]; then
+                break
+            else
+                echo "输入不合法，请重新输入"
+            fi
+        done
+    fi
+
+    if [ ! -f "$MYSQL_SLAVE_2_CNF" ]; then
+        mkdir -p $HOME/mysql/mysql-slave-2/conf
+        cp -f $PROJECT/mysql-slave-2.cnf $HOME/mysql/mysql-slave-2/conf/my.cnf
+    else
+        while true; do
+            read -p "mysql-slave-2 的 my.cnf 文件已存在，如需覆盖请确认？（y/n）" choice
+            choice_lower=$(echo "$choice" | tr '[:upper:]' '[:lower:]')
+            if [ "$choice_lower" == "y" ] || [ "$choice_lower" == "yes" ]; then
+                cp -f $PROJECT/mysql-slave-2.cnf $HOME/mysql/mysql-slave-2/conf/my.cnf
+                break
+            elif [ "$choice_lower" == "n" ] || [ "$choice_lower" == "no" ]; then
+                break
+            else
+                echo "输入不合法，请重新输入"
+            fi
+        done
+    fi
+fi
 
 # 创建mysql-master
 docker run \
     -itd \
     --name mysql-master \
     -p 3306:3306 \
-    -v /Users/zhaohongliang/DockerData/mysql/mysql-master/conf:/etc/mysql/conf.d \
-    -v /Users/zhaohongliang/DockerData/mysql/mysql-master/data:/var/lib/mysql \
-    -v /Users/zhaohongliang/DockerData/mysql/mysql-master/log:/var/log/mysql \
+    -v $HOME/mysql/mysql-master/conf:/etc/mysql/conf.d \
+    -v $HOME/mysql/mysql-master/data:/var/lib/mysql \
+    -v $HOME/mysql/mysql-master/log:/var/log/mysql \
     -v /etc/localtime:/etc/localtime \
     -e MYSQL_ROOT_PASSWORD=123456   \
     --restart no \
@@ -49,16 +187,24 @@ docker run \
     --network canary-net \
     mysql
 
-sleep 5
+progress 100 "MySQL(Master)"
+
+while ! docker ps | grep mysql-master; do
+    for i in $(seq 0 3); do
+        echo -en "\rmysql-master 正在启动...${spinner:$i:1}"
+        sleep 0.2
+    done
+done
+printf "Master 启动成功！\n"
 
 # 创建mysql-slave-1
 docker run \
     -itd \
     --name mysql-slave-1 \
     -p 3307:3306 \
-    -v /Users/zhaohongliang/DockerData/mysql/mysql-slave-1/conf:/etc/mysql/conf.d \
-    -v /Users/zhaohongliang/DockerData/mysql/mysql-slave-1/data:/var/lib/mysql \
-    -v /Users/zhaohongliang/DockerData/mysql/mysql-slave-1/log:/var/log/mysql \
+    -v $HOME/mysql/mysql-slave-1/conf:/etc/mysql/conf.d \
+    -v $HOME/mysql/mysql-slave-1/data:/var/lib/mysql \
+    -v $HOME/mysql/mysql-slave-1/log:/var/log/mysql \
     -v /etc/localtime:/etc/localtime \
     -e MYSQL_ROOT_PASSWORD=123456   \
     --restart no \
@@ -66,16 +212,24 @@ docker run \
     --network canary-net \
     mysql
 
-sleep 5
+progress 100 "MySQL(Slave-1)"
+
+while ! docker ps | grep mysql-slave-1; do
+    for i in $(seq 0 3); do
+        echo -en "\rmysql-slave-2 正在启动...${spinner:$i:1}"
+        sleep 0.2
+    done
+done
+printf "Slave-1 启动成功！\n"
 
 # 创建mysql-slave-2
 docker run \
     -itd \
     --name mysql-slave-2 \
     -p 3308:3306 \
-    -v /Users/zhaohongliang/DockerData/mysql/mysql-slave-2/conf:/etc/mysql/conf.d \
-    -v /Users/zhaohongliang/DockerData/mysql/mysql-slave-2/data:/var/lib/mysql \
-    -v /Users/zhaohongliang/DockerData/mysql/mysql-slave-2/log:/var/log/mysql \
+    -v $HOME/mysql/mysql-slave-2/conf:/etc/mysql/conf.d \
+    -v $HOME/mysql/mysql-slave-2/data:/var/lib/mysql \
+    -v $HOME/mysql/mysql-slave-2/log:/var/log/mysql \
     -v /etc/localtime:/etc/localtime \
     -e MYSQL_ROOT_PASSWORD=123456   \
     --restart no \
@@ -83,57 +237,72 @@ docker run \
     --network canary-net \
     mysql
 
-MYSQL_MASTER_CNF="/Users/zhaohongliang/DockerData/mysql/mysql-master/conf/my.cnf"
-MYSQL_SLAVE_1_CNF="/Users/zhaohongliang/DockerData/mysql/mysql-slave-1/conf/my.cnf"
-MYSQL_SLAVE_2_CNF="/Users/zhaohongliang/DockerData/mysql/mysql-slave-2/conf/my.cnf"
+progress 100 "MySQL(Slave-2)"
 
-if [ ! -f "$MYSQL_MASTER_CNF" ];
+while ! docker ps | grep mysql-slave-2; do
+    for i in $(seq 0 3); do
+        echo -en "\rmysql-slave-2 正在启动...${spinner:$i:1}"
+        sleep 0.2
+    done
+done
+printf "Slave-2 启动成功！\n"
+
+# docker restart mysql-master && \
+# docker restart mysql-slave-1 && \
+# docker restart mysql-slave-2
+
+PROXYSQL_CNF=$HOME/proxysql
+if [ ! -f "$PROXYSQL_CNF/proxsql.cnf" ];
 then
-    # wget -P /Users/zhaohongliang/DockerData/mysql/mysql-master/conf  https://github.com/hahapigs/canary/blob/main/mysql-master.cnf 
-    cp /Users/zhaohongliang/GitHub/canary/mysql-master.cnf /Users/zhaohongliang/DockerData/mysql/mysql-master/conf
-    mv /Users/zhaohongliang/DockerData/mysql/mysql-master/conf/mysql-master.cnf   /Users/zhaohongliang/DockerData/mysql/mysql-master/conf/my.cnf
+    # wget -P $HOME/proxysql https://github.com/hahapigs/canary/blob/main/proxysql.cnf
+    mkdir -p $HOME/proxysql
+    cp $PROJECT/proxysql.cnf $HOME/proxysql
 fi
 
-if [ ! -f "$MYSQL_SLAVE_1_CNF" ];
-then
-    # wget -P /Users/zhaohongliang/DockerData/mysql/mysql-slave-1/conf https://github.com/hahapigs/canary/blob/main/mysql-slave-1.cnf
-    cp /Users/zhaohongliang/GitHub/canary/mysql-slave-1.cnf /Users/zhaohongliang/DockerData/mysql/mysql-slave-1/conf
-    mv /Users/zhaohongliang/DockerData/mysql/mysql-slave-1/conf/mysql-slave-1.cnf /Users/zhaohongliang/DockerData/mysql/mysql-slave-1/conf/my.cnf
+docker run -p 16032:6032 -p 16033:6033 -p 16070:6070 --name proxysql --network canary-net -d -v $HOME/proxysql/proxysql.cnf:/etc/proxysql.cnf proxysql/proxysql
+
+b=' '
+i=0
+while [ $i -le 100  ]
+do
+    printf "ProxySQL：[%-100s] %d%%\r" $b $i
+    sleep 0.1
+    i=`expr 1 + $i`
+    b=#$b
+done
+printf "\nProxySQL 构建完成！\n"
+
+while ! docker ps | grep mysql-slave-1; do
+    for i in $(seq 0 3); do
+        echo -en "\rproxysql 正在启动...${spinner:$i:1}"
+        sleep 0.2
+    done
+done
+printf "ProxySQL 启动成功！\n"
+
+nc -z -v -w 3 127.0.0.1 3306
+# 检查连接状态
+if [ $? -eq 0  ]; then
+    echo "Master 数据库连接正常"
+else
+    echo "Master 数据库连接失败"
 fi
 
-if [ ! -f "$MYSQL_SLAVE_2_CNF" ];
-then
-    # wget -P /Users/zhaohongliang/DockerData/mysql/mysql-slave-2/conf https://github.com/hahapigs/canary/blob/main/mysql-slave-2.cnf
-    cp /Users/zhaohongliang/GitHub/canary/mysql-slave-2.cnf /Users/zhaohongliang/DockerData/mysql/mysql-slave-2/conf
-    mv /Users/zhaohongliang/DockerData/mysql/mysql-slave-2/conf/mysql-slave-2.cnf /Users/zhaohongliang/DockerData/mysql/mysql-slave-2/conf/my.cnf
+nc -z -v -w 3 127.0.0.1 3306
+# 检查连接状态
+if [ $? -eq 0  ]; then
+    echo "Slave-1 数据库连接正常"
+else
+    echo "Slave-1 数据库连接失败"
 fi
 
-sleep 2
-docker restart mysql-master
-sleep 2
-docker restart mysql-slave-1
-sleep 2
-docker restart mysql-slave-2
-sleep 2
- 
-# 镜像名称
-IMAGE_NAME_PROXYSQL="proxysql/proxysql"
-
-# 如果proxy镜像不存在，则拉取
-if !docker image inspect $IMAGE_NAME_PROXYSQL &> /dev/null
-then
-    docker pull proxysql/proxysql
+nc -z -v -w 3 127.0.0.1 3306
+# 检查连接状态
+if [ $? -eq 0  ]; then
+    echo "Slave-2 数据库连接正常"
+else
+    echo "Slave-2 数据库连接失败"
 fi
-
-if [ ! -f "$MYSQL_SLAVE_2_CNF" ];
-then
-    # wget -P /Users/zhaohongliang/DockerData/proxysql https://github.com/hahapigs/canary/blob/main/proxysql.cnf
-    cp /Users/zhaohongliang/GitHub/canary/proxysql.cnf /Users/zhaohongliang/DockerData/proxysql
-fi
-
-docker run -p 16032:6032 -p 16033:6033 -p 16070:6070 --name proxysql --network canary-net -d -v /Users/zhaohongliang/DockerData/proxysql/proxysql.cnf:/etc/proxysql.cnf proxysql/proxysql
-
-sleep 5
 
 # 数据库连接信息
 DB_USER="root"
@@ -147,26 +316,34 @@ FRONT_PART=${MASTER_IP:0:$(($MASTER_IP_LENGTH-1))}
 LAST_CHAR=${MASTER_IP:-1}
 NEW_MASTER_IP="${FRONT_PART}%"
 
+dynamic_cursor 10 "Master 正在创建主从复制账号..."
+
+# 创建主从同步复制用户
 mysql -h 127.0.0.1 -P 3306 -u$DB_USER -p$DB_PASS <<EOF
 CREATE USER 'replication.user'@'$NEW_MASTER_IP' IDENTIFIED WITH mysql_native_password BY 'Copy!234';
 GRANT REPLICATION SLAVE ON *.* TO 'replication.user'@'$NEW_MASTER_IP';
 FLUSH PRIVILEGES;
 EOF
-sleep 2
 
+dynamic_cursor 10 "Slave-1 开启复制..."
+
+# log-file
 MASTER_LOG_FILE=$(mysql -h 127.0.0.1 -P 3306 -u $DB_USER -p$DB_PASS -e "SHOW MASTER STATUS\G" | grep "File" | awk '{print $2}')
+# log-pos
 MASTER_LOG_POS=$(mysql -h 127.0.0.1 -P 3306 -u $DB_USER -p$DB_PASS -e "SHOW MASTER STATUS\G" | grep "Position" | awk '{print $2}')
+# 开启 slave-1 主从复制
 mysql -h 127.0.0.1 -P 3307 -u $DB_USER -p$DB_PASS <<EOF
 CHANGE MASTER TO MASTER_HOST='$MASTER_IP', MASTER_USER='replication.user', MASTER_PASSWORD='Copy!234', MASTER_LOG_FILE='$MASTER_LOG_FILE', MASTER_LOG_POS=$MASTER_LOG_POS;
 START SLAVE;
 EOF
-sleep 2
 
+dynamic_cursor 10 "Slave-2 开启复制..."
+
+# 开启 slave-2 主从复制
 mysql -h 127.0.0.1 -P 3308 -u $DB_USER -p$DB_PASS <<EOF
 CHANGE MASTER TO MASTER_HOST='$MASTER_IP', MASTER_USER='replication.user', MASTER_PASSWORD='Copy!234', MASTER_LOG_FILE='$MASTER_LOG_FILE', MASTER_LOG_POS=$MASTER_LOG_POS;
 START SLAVE;
 EOF
-sleep 2
 
 # SLAVE_1_IO=$(mysql -h 127.0.0.1 -P 3307 -u $DB_USER -p$DB_PASS -e "SHOW SLAVE STATUS\G" | grep "Slave_IO_Running" | awk '{print $2}')
 # SLAVE_2_IO=$(mysql -h 127.0.0.1 -P 3308 -u $DB_USER -p$DB_PASS -e "SHOW SLAVE STATUS\G" | grep "Slave_IO_Running" | awk '{print $2}')
@@ -190,29 +367,27 @@ sleep 2
 #     # exit 1
 # fi
 
-
- 
+dynamic_cursor 10 "修改主从数据库root账号信息..."
 
 mysql -h 127.0.0.1 -P 3306 -u $DB_USER -p$DB_PASS <<EOF
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'Pass!234';
 ALTER USER 'root'@'%' IDENTIFIED BY 'Pass!234';
 FLUSH PRIVILEGES;
 EOF
-sleep 5
+
 # mysql -h 127.0.0.1 -P 3307 -u $DB_USER -p$DB_PASS <<EOF
 # ALTER USER 'root'@'localhost' IDENTIFIED BY 'Pass!234';
 # ALTER USER 'root'@'%' IDENTIFIED BY 'Pass!234';
 # FLUSH PRIVILEGES;
 # EOF
-# sleep 5
+
 # mysql -h 127.0.0.1 -P 3308 -u $DB_USER -p$DB_PASS <<EOF
 # ALTER USER 'root'@'localhost' IDENTIFIED BY 'Pass!234';
 # ALTER USER 'root'@'%' IDENTIFIED BY 'Pass!234';
 # FLUSH PRIVILEGES;
 # EOF
-# sleep 5
 
-echo "====="
+dynamic_cursor 10 "正在创建监测用户和外部访问用户..."
 
 mysql -h 127.0.0.1 -P 3306 -u $DB_USER -p$DB_NEW_PASS <<EOF
 create user 'proxy.monitor'@'$NEW_MASTER_IP' identified by '123456';
@@ -221,7 +396,6 @@ create user 'proxy.admin'@'%' identified by 'Pass!234';
 grant all privileges on *.* to 'proxy.admin'@'%' with grant option;
 FLUSH PRIVILEGES;
 EOF
-sleep 5
 
 # mysql -h 127.0.0.1 -P 3307 -u $DB_USER -p$DB_NEW_PASS <<EOF
 # create user 'proxy.monitor'@'$NEW_MASTER_IP' identified by '123456';
@@ -230,8 +404,7 @@ sleep 5
 # grant all privileges on *.* to 'proxy.admin'@'%' with grant option;
 # FLUSH PRIVILEGES;
 # EOF
-# sleep 5
-# 
+
 # mysql -h 127.0.0.1 -P 3308 -u $DB_USER -p$DB_NEW_PASS <<EOF 
 # create user 'proxy.monitor'@'$NEW_MASTER_IP' identified by '123456';
 # grant replication client on *.* to 'proxy.monitor'@'$NEW_MASTER_IP';
@@ -239,15 +412,22 @@ sleep 5
 # grant all privileges on *.* to 'proxy.admin'@'%' with grant option;
 # FLUSH PRIVILEGES;
 # EOF
-# sleep 5
-echo "======"
+
+# mysql -h 127.0.0.1 -P 3306 -u $DB_USER -p$DB_NEW_PASS -e "CREATE DATABASE IF NOT EXISTS canary DEFAULT CHARACTER SET = utf8mb4;"
+# 
+# if [ $? -eq 0 ]; then
+#     mysql -h 127.0.0.1 -P 3306 -u $DB_USER -p$DB_NEW_PASS canary < $SQL/canary.sql
+# fi
+
+dynamic_cursor 10 "正在添加ProxySQL主从分组信息..."
 
 mysql -h 127.0.0.1 -P 16032 -uradmin -pradmin --prompt "ProxySQL Admin>" <<EOF
-insert into mysql_replication_hostgroups(writer_hostgroup,reader_hostgroup,check_type, COMMENT) values(10,20,'read_only', 'proxy');
+insert into mysql_replication_hostgroups(writer_hostgroup,reader_hostgroup,check_type, COMMENT) values(10, 20,'read_only', 'proxy');
 load mysql servers to runtime;
 save mysql servers to disk;
 EOF
-sleep 5
+
+dynamic_cursor 10 "正在添加ProxySQL主从服务器的节点..."
 
 SLAVE_1_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mysql-slave-1)
 SLAVE_2_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mysql-slave-2)
@@ -256,8 +436,8 @@ insert into mysql_servers(hostgroup_id,hostname,port)  values(10,'$MASTER_IP',33
 load mysql servers to runtime;
 save mysql servers to disk;
 EOF
-sleep 5
 
+dynamic_cursor 10 "正在配置ProxySQL监测账号..."
 
 mysql -h 127.0.0.1 -P 16032 -uradmin -pradmin --prompt "ProxySQL Admin>" <<EOF
 UPDATE global_variables SET variable_value='proxy.monitor' WHERE variable_name='mysql-monitor_username';
@@ -265,7 +445,8 @@ UPDATE global_variables SET variable_value='123456' WHERE variable_name='mysql-m
 load mysql variables to runtime;
 save mysql variables to disk;
 EOF
-sleep 5
+
+dynamic_cursor 10 "正在配置ProxySQL访问账号..."
 
 mysql -h 127.0.0.1 -P 16032 -uradmin -pradmin --prompt "ProxySQL Admin>" << EOF
 insert into mysql_users(username,password,default_hostgroup) values('proxy.admin','Pass!234',10);
@@ -273,11 +454,11 @@ load mysql users to runtime;
 save mysql users to disk;
 EOF
 
+dynamic_cursor 10 "正在创建路由规则..."
+
 mysql -h 127.0.0.1 -P 16032 -uradmin -pradmin --prompt "ProxySQL Admin>" << EOF
 INSERT INTO mysql_query_rules (rule_id, active, match_pattern, destination_hostgroup, apply) VALUES (1, 1, '^select', 20, 1),(2, 1, '^select.*for update$', 10, 1),(3, 1, '.*', 10, 1);
 load mysql query rules to runtime;
 save mysql query rules to disk;
 EOF
-
-
 
