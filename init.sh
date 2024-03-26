cddf5036b3dd15789a992f0e76972091dc8f3a98
hongliang <15711385916@163.com>
2024-03-27 01:43:34 +0800

    feat:初始化脚本优化

--- init.sh
+++ init.sh
@@ -7,33 +7,54 @@ PROJECT="/Users/zhaohongliang/Github/canary"
 # sql 文件根目录
 SQL="/Users/zhaohongliang/Desktop"
 
+
+# ANSI 转义码定义
+RED='\033[0;31m'    # 红色字体
+GREEN='\033[0;32m'  # 绿色字体
+BOLD='\033[1m'      # 加粗字体
+NC='\033[0m'        # 恢复默认格式
+
 # 定义旋转的光标符号
 spinner="/-\|"
 
-# 旋转光标
+# 旋转光标（等待）
 dynamic_cursor() {
     local j=0
-    local max=$1
-    local str=$2
+    local max=5
 
     while [ $j -le $max ]; do
         for i in $(seq 0 3); do
-            printf "\r${str}${spinner:$i:1}"   # 使用回车符（\r）实现光标回到行首
+            printf "\r正在执行...${spinner:$i:1}"   # 使用回车符（\r）实现光标回到行首
             sleep 0.2
         done
         j=`expr 1 + $j`
     done
     printf "\r\033[K"
-    printf "\n执行完毕！\n"
+    printf "执行完毕！\n"
+}
+
+# 旋转光标（校验）
+is_load() {
+    local str=$1
+
+    while ! docker ps | grep $1; do
+        for i in $(seq 0 3); do
+            printf "\r${1} 正在启动...${spinner:$i:1}"
+            sleep 0.2
+        done
+    done
+    printf "${1} 启动成功！\n"
+    echo ""
+    
 }
 
 # 进度条
 progress() {
     local i=0
     local b=' '
-    local max=$1
-    local str=$2
-
+    local max=100
+    local str=$1
+    
     while [ $i -le $max  ]; do
         printf "${str}：[%-100s] %d%%\r" $b $i
         sleep 0.1
@@ -51,14 +72,14 @@ COMMAND_MYSQL="mysql"
 # 检测 docker 命令是否存在
 if !command -v $COMMAND_DOCKER &> /dev/null
 then
-    echo "$COMMAND_DOCKER 未安装，脚本中断执行"
+    printf "$COMMAND_DOCKER 未安装，脚本中断执行"
     exit 1
 fi
 
 # 检测 mysql 命令是否存在
 if !command -v $COMMAND_MYSQL &> /dev/null
 then
-    echo "$COMMAND_DOCKER 未安装，脚本中断执行"
+    printf "$COMMAND_DOCKER 未安装，脚本中断执行"
     exit 1
 fi
 
@@ -72,7 +93,7 @@ if !docker image inspect $IMAGE_NAME_MYSQL &> /dev/null
 then
     docker pull mysql:8.0
     if [ $? -eq 0 ]; then
-        echo "$IMAGE_NAME_MYSQL 拉取完成"
+        printf "$IMAGE_NAME_MYSQL 拉取完成"
     fi
 fi
 
@@ -81,7 +102,7 @@ if !docker image inspect $IMAGE_NAME_PROXYSQL &> /dev/null
 then
     docker pull proxysql/proxysql
     if [ $? -eq 0 ]; then
-        echo "$IMAGE_NAME_PROXYSQL 拉取完成"
+        printf "$IMAGE_NAME_PROXYSQL 拉取完成"
     fi
 fi
 
@@ -92,14 +113,14 @@ NETWORK="canary-net"
 if docker network ls | grep -q $NETWORK &> /dev/null; then
     while true; do
         read -p "$NETWORK 已存在，如需覆盖请确认？（y/n）" choice
-        choice_lower=$(echo "$choice" | tr '[:upper:]' '[:lower:]')
+        choice_lower=$(printf "$choice" | tr '[:upper:]' '[:lower:]')
         if [ "$choice_lower" == "y" ] || [ "$choice_lower" == "yes" ]; then
             docker network rm $NETWORK &> /dev/null && docker network create $NETWORK &> /dev/null
             break
         elif [ "$choice_lower" == "n" ] || [ "$choice_lower" == "no" ]; then
             break
         else
-            echo "输入不合法，请重新输入"
+            printf "输入不合法，请重新输入"
         fi
     done
 else
@@ -122,7 +143,7 @@ if [ ! -d "$MYSQL_HOME" ]; then
 else
     while true; do
         read -p "是否需要覆盖 MySQL 配置？（y/n）" choice
-        choice_lower=$(echo "$choice" | tr '[:upper:]' '[:lower:]')
+        choice_lower=$(printf "$choice" | tr '[:upper:]' '[:lower:]')
         if [ "$choice_lower" == "y" ] || [ "$choice_lower" == "yes" ]; then
             rm -rf $MYSQL_HOME &&
             mkdir -p $MASTER_HOME/conf && mkdir -p $SLAVE_1_HOME/conf && mkdir -p $SLAVE_2_HOME/conf
@@ -133,7 +154,7 @@ else
         elif [ "$choice_lower" == "n" ] || [ "$choice_lower" == "no" ]; then
             break
         else
-            echo "输入不合法，请重新输入"
+            printf "输入不合法，请重新输入"
         fi
     done
 fi
@@ -153,15 +174,8 @@ docker run \
     --network $NETWORK \
     mysql &> /dev/null
 
-progress 100 "MySQL(Master)"
-
-while ! docker ps | grep mysql-master; do
-    for i in $(seq 0 3); do
-        printf "\rmysql-master 正在启动...${spinner:$i:1}"
-        sleep 0.2
-    done
-done
-printf "Master 启动成功！\n"
+progress "MySQL(Master)"
+is_load "mysql-master"
 
 # 创建mysql-slave-1
 docker run \
@@ -178,15 +192,8 @@ docker run \
     --network $NETWORK \
     mysql &> /dev/null
 
-progress 100 "MySQL(Slave-1)"
-
-while ! docker ps | grep mysql-slave-1; do
-    for i in $(seq 0 3); do
-        printf "\rmysql-slave-2 正在启动...${spinner:$i:1}"
-        sleep 0.2
-    done
-done
-printf "Slave-1 启动成功！\n"
+progress "MySQL(Slave-1)"
+is_load "mysql-slave-1"
 
 # 创建mysql-slave-2
 docker run \
@@ -203,15 +210,8 @@ docker run \
     --network $NETWORK \
     mysql &> /dev/null
 
-progress 100 "MySQL(Slave-2)"
-
-while ! docker ps | grep mysql-slave-2; do
-    for i in $(seq 0 3); do
-        printf "\rmysql-slave-2 正在启动...${spinner:$i:1}"
-        sleep 0.2
-    done
-done
-printf "Slave-2 启动成功！\n"
+progress "MySQL(Slave-2)"
+is_load "mysql-slave-2"
 
 # proxysql
 PROXYSQL_HOME=$HOME/proxysql
@@ -222,8 +222,9 @@ if [ ! -d "$PROXYSQL_HOME" ]; then
     cp $PROJECT/proxysql.cnf $PROXYSQL_HOME
 else
     while true; do
+        echo ""
         read -p "是否需要覆盖 ProxySQL 配置？（y/n）" choice
-        choice_lower=$(echo "$choice" | tr '[:upper:]' '[:lower:]')
+        choice_lower=$(printf "$choice" | tr '[:upper:]' '[:lower:]')
         if [ "$choice_lower" == "y" ] || [ "$choice_lower" == "yes" ]; then
             rm -rf $PROXYSQL_HOME &&
             mkdir -p $PROXYSQL_HOME &&
@@ -232,7 +233,7 @@ else
         elif [ "$choice_lower" == "n" ] || [ "$choice_lower" == "no" ]; then
             break
         else
-            echo "输入不合法，请重新输入"
+            printf "输入不合法，请重新输入"
         fi
     done
 fi
@@ -240,7 +241,7 @@ fi
 # 创建 proxsql
 docker run -p 16032:6032 -p 16033:6033 -p 16070:6070 --name proxysql --network $NETWORK -d -v $PROXYSQL_HOME/proxysql.cnf:/etc/proxysql.cnf proxysql/proxysql &> /dev/null
 
-progress 100 "MySQL(ProxySQL)"
+progress "MySQL(ProxySQL)"
 
 while ! docker ps | grep proxysql; do
     for i in $(seq 0 3); do
@@ -248,29 +249,29 @@ while ! docker ps | grep proxysql; do
         sleep 0.2
     done
 done
-printf "ProxySQL 启动成功！\n"
+printf "ProxySQL 启动成功！"
 
 # 检查数据库连接状态
 # master
 nc -z -v -w 3 127.0.0.1 3306
 if [ $? -eq 0  ]; then
-    echo "Master 数据库连接正常！"
+    printf "Master 数据库连接正常！\n"
 else
-    echo "Master 数据库连接失败！"
+    printf "Master 数据库连接失败！\n"
 fi
 # slave-1
 nc -z -v -w 3 127.0.0.1 3307
 if [ $? -eq 0  ]; then
-    echo "Slave-1 数据库连接正常！"
+    printf "Slave-1 数据库连接正常！\n"
 else
-    echo "Slave-1 数据库连接失败！"
+    printf "Slave-1 数据库连接失败！\n"
 fi
 # slave-2
 nc -z -v -w 3 127.0.0.1 3308
 if [ $? -eq 0  ]; then
-    echo "Slave-2 数据库连接正常！"
+    printf "Slave-2 数据库连接正常！\n"
 else
-    echo "Slave-2 数据库连接失败！"
+    printf "Slave-2 数据库连接失败！\n"
 fi
 
 # 数据库连接信息
@@ -286,34 +287,31 @@ LAST_CHAR=${MASTER_IP:-1}
 NEW_MASTER_IP="${FRONT_PART}%"
 
 
-# 创建主从同步复制用户
-dynamic_cursor 10 "Master 正在创建主从复制账号..."
-
+printf "\n==>${BOLD} 创建主从复制账号${NC}\n"
 mysql -h 127.0.0.1 -P 3306 -u$DB_USER -p$DB_PASS <<EOF
 CREATE USER 'replication.user'@'$NEW_MASTER_IP' IDENTIFIED WITH mysql_native_password BY 'Copy!234';
 GRANT REPLICATION SLAVE ON *.* TO 'replication.user'@'$NEW_MASTER_IP';
 FLUSH PRIVILEGES;
 EOF
+dynamic_cursor
 
-dynamic_cursor 10 "Slave-1 开启复制..."
-
+printf "\n==>${BOLD} 开启 Slave-1 主从复制${NC}\n"
 # log-file
 MASTER_LOG_FILE=$(mysql -h 127.0.0.1 -P 3306 -u $DB_USER -p$DB_PASS -e "SHOW MASTER STATUS\G" | grep "File" | awk '{print $2}')
 # log-pos
 MASTER_LOG_POS=$(mysql -h 127.0.0.1 -P 3306 -u $DB_USER -p$DB_PASS -e "SHOW MASTER STATUS\G" | grep "Position" | awk '{print $2}')
-# 开启 slave-1 主从复制
 mysql -h 127.0.0.1 -P 3307 -u $DB_USER -p$DB_PASS <<EOF
 CHANGE MASTER TO MASTER_HOST='$MASTER_IP', MASTER_USER='replication.user', MASTER_PASSWORD='Copy!234', MASTER_LOG_FILE='$MASTER_LOG_FILE', MASTER_LOG_POS=$MASTER_LOG_POS;
 START SLAVE;
 EOF
+dynamic_cursor
 
-dynamic_cursor 10 "Slave-2 开启复制..."
-
-# 开启 slave-2 主从复制
+printf "\n==>${BOLD} 开启 slave-2 主从复制${NC}\n"
 mysql -h 127.0.0.1 -P 3308 -u $DB_USER -p$DB_PASS <<EOF
 CHANGE MASTER TO MASTER_HOST='$MASTER_IP', MASTER_USER='replication.user', MASTER_PASSWORD='Copy!234', MASTER_LOG_FILE='$MASTER_LOG_FILE', MASTER_LOG_POS=$MASTER_LOG_POS;
 START SLAVE;
 EOF
+dynamic_cursor
 
 # SLAVE_1_IO=$(mysql -h 127.0.0.1 -P 3307 -u $DB_USER -p$DB_PASS -e "SHOW SLAVE STATUS\G" | grep "Slave_IO_Running" | awk '{print $2}')
 # SLAVE_2_IO=$(mysql -h 127.0.0.1 -P 3308 -u $DB_USER -p$DB_PASS -e "SHOW SLAVE STATUS\G" | grep "Slave_IO_Running" | awk '{print $2}')
@@ -321,29 +319,29 @@ EOF
 # SLAVE_2_SQL=$(mysql -h 127.0.0.1 -P 3308 -u $DB_USER -p$DB_PASS -e "SHOW SLAVE STATUS\G" | grep "Slave_SQL_Running" | awk '{print $2}')
 # 
 # if [ "$SLAVE_1_IO" != "Yes" ]; then
-#     echo "slave-1 io: $SLAVE_1_IO"
+#     printf "slave-1 io: $SLAVE_1_IO"
 #     # exit 1
 # fi
 # if [ "$SLAVE_1_SQL" != "Yes" ]; then
-#     echo "slave-1 sql: $SLAVE_1_SQL"
+#     printf "slave-1 sql: $SLAVE_1_SQL"
 #     # exit 1
 # fi
 # if [ "$SLAVE_2_IO" != "Yes" ]; then
-#     echo "slave-2 io: $SLAVE_2_IO"
+#     printf "slave-2 io: $SLAVE_2_IO"
 #     # exit 1
 # fi
 # if [ "$SLAVE_2_SQL" != "Yes" ]; then
-#     echo "slave-2 sql: $SLAVE_2_SQL"
+#     printf "slave-2 sql: $SLAVE_2_SQL"
 #     # exit 1
 # fi
 
-dynamic_cursor 10 "修改主从数据库root账号信息..."
-
+printf "\n==>${BOLD} 修改主从数据库root账号信息${NC}\n"
 mysql -h 127.0.0.1 -P 3306 -u $DB_USER -p$DB_PASS <<EOF
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'Pass!234';
 ALTER USER 'root'@'%' IDENTIFIED BY 'Pass!234';
 FLUSH PRIVILEGES;
 EOF
+dynamic_cursor
 
 # mysql -h 127.0.0.1 -P 3307 -u $DB_USER -p$DB_PASS <<EOF
 # ALTER USER 'root'@'localhost' IDENTIFIED BY 'Pass!234';
@@ -357,8 +355,7 @@ EOF
 # FLUSH PRIVILEGES;
 # EOF
 
-dynamic_cursor 10 "正在创建监测用户和外部访问用户..."
-
+printf "\n==>${BOLD} 创建监测用户和外部访问用户${NC}\n"
 mysql -h 127.0.0.1 -P 3306 -u $DB_USER -p$DB_NEW_PASS <<EOF
 create user 'proxy.monitor'@'$NEW_MASTER_IP' identified by '123456';
 grant replication client on *.* to 'proxy.monitor'@'$NEW_MASTER_IP';
@@ -366,6 +363,7 @@ create user 'proxy.admin'@'%' identified by 'Pass!234';
 grant all privileges on *.* to 'proxy.admin'@'%' with grant option;
 FLUSH PRIVILEGES;
 EOF
+dynamic_cursor
 
 # mysql -h 127.0.0.1 -P 3307 -u $DB_USER -p$DB_NEW_PASS <<EOF
 # create user 'proxy.monitor'@'$NEW_MASTER_IP' identified by '123456';
@@ -389,46 +387,46 @@ EOF
 #     mysql -h 127.0.0.1 -P 3306 -u $DB_USER -p$DB_NEW_PASS canary < $SQL/canary.sql
 # fi
 
-dynamic_cursor 10 "正在添加ProxySQL主从分组信息..."
-
+printf "\n==>${BOLD} 添加ProxySQL主从分组信息${NC}\n"
 mysql -h 127.0.0.1 -P 16032 -uradmin -pradmin --prompt "ProxySQL Admin>" <<EOF
 insert into mysql_replication_hostgroups(writer_hostgroup,reader_hostgroup,check_type, COMMENT) values(10, 20,'read_only', 'proxy');
 load mysql servers to runtime;
 save mysql servers to disk;
 EOF
-
-dynamic_cursor 10 "正在添加ProxySQL主从服务器的节点..."
+dynamic_cursor
 
 SLAVE_1_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mysql-slave-1)
 SLAVE_2_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mysql-slave-2)
+
+printf "\n==>${BOLD} 添加ProxySQL主从服务器的节点${NC}\n"
 mysql -h 127.0.0.1 -P 16032 -uradmin -pradmin --prompt "ProxySQL Admin>" <<EOF
 insert into mysql_servers(hostgroup_id,hostname,port)  values(10,'$MASTER_IP',3306),(20,'$SLAVE_1_IP',3306),(20,'$SLAVE_2_IP',3306);
 load mysql servers to runtime;
 save mysql servers to disk;
 EOF
+dynamic_cursor
 
-dynamic_cursor 10 "正在配置ProxySQL监测账号..."
-
+printf "\n==>${BOLD} 配置ProxySQL监测账号${NC}\n"
 mysql -h 127.0.0.1 -P 16032 -uradmin -pradmin --prompt "ProxySQL Admin>" <<EOF
 UPDATE global_variables SET variable_value='proxy.monitor' WHERE variable_name='mysql-monitor_username';
 UPDATE global_variables SET variable_value='123456' WHERE variable_name='mysql-monitor_password';
 load mysql variables to runtime;
 save mysql variables to disk;
 EOF
+dynamic_cursor
 
-dynamic_cursor 10 "正在配置ProxySQL访问账号..."
-
+printf "\n==>${BOLD} 配置ProxySQL访问账号${NC}\n"
 mysql -h 127.0.0.1 -P 16032 -uradmin -pradmin --prompt "ProxySQL Admin>" << EOF
 insert into mysql_users(username,password,default_hostgroup) values('proxy.admin','Pass!234',10);
 load mysql users to runtime;
 save mysql users to disk;
 EOF
+dynamic_cursor
 
-dynamic_cursor 10 "正在创建路由规则..."
-
+printf "\n==>${BOLD} 创建路由规则${NC}\n"
 mysql -h 127.0.0.1 -P 16032 -uradmin -pradmin --prompt "ProxySQL Admin>" << EOF
 INSERT INTO mysql_query_rules (rule_id, active, match_pattern, destination_hostgroup, apply) VALUES (1, 1, '^select', 20, 1),(2, 1, '^select.*for update$', 10, 1),(3, 1, '.*', 10, 1);
 load mysql query rules to runtime;
 save mysql query rules to disk;
 EOF
-
+dynamic_cursor
